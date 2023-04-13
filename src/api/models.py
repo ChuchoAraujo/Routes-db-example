6d2418d57cf7964c78b54f6069ca5d23d6dde635
ChuchoAraujo <jeseazuara@gmail.com>
2023-04-13 13:52:37 +0000

    tabla people

--- src/api/models.py
+++ src/api/models.py
@@ -20,3 +20,20 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(180), unique=False, nullable=False)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            # do not serialize the password, its a security breach
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, People
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -15,7 +15,7 @@ def create_user():
     new_user = User(body['user_name'], body['email'], body['password'])
     db.session.add(new_user)
     db.session.commit()
-    return jsonify(new_user.serialize()), 200
+    return jsonify({'user': new_user.serialize()}), 200
 
 @api.route('/user', methods=['GET'])
 def get_all_users():
@@ -26,7 +26,10 @@ def get_all_users():
 @api.route('/user/<int:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
     one_user = User.query.get(user_id)
-    return jsonify(one_user.serialize()), 200
+    if one_user:
+        return jsonify({'user': one_user.serialize()}), 200
+    else:
+        return jsonify({'msg': 'Id not exist'})
 
 @api.route('/user/<int:user_id>', methods=['DELETE'])
 def delete_user_by_id(user_id):
@@ -37,3 +40,34 @@ def delete_user_by_id(user_id):
         return jsonify({"msg": user.serialize()}), 200
     else:
         return jsonify({"error": "User not found"}), 404
+
+############################# TABLA USER ############################################
+@api.route('/people', methods=['GET'])
+def get_people():
+    all_people = People.query.all()
+    all_people_serializable = list(map(lambda people: people.serialize(), all_people))
+    return jsonify({'people': all_people_serializable})
+
+@api.route('/people', methods=['POST'])
+def create_people():
+    body = request.get_json()
+    new_people = People(body['name'], body['description'])
+    db.session.add(new_people)
+    db.session.commit()
+    return jsonify({'people': new_people.serialize()})
+
+@api.route('/people/<int:people_id>', methods=['GET'])
+def get_one_people(people_id):
+    one_people = People.query.get(people_id)
+    if one_people:
+        return jsonify({'people': one_people.serialize()}), 200
+    else:
+        return jsonify({'msg': 'Id not exist!'})
+
+api.route('/people/<int:people_id>', methods=['DELETE'])
+def delete_people(people_id):
+    one_people = People.query.get(people_id)
+    if one_people:
+        db.session.delete(one_people)
+        db.session.commit()
+        return jsonify({'people': one_people.serialize})
