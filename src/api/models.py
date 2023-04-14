b99c175b5ca1a8a9aed30999ac687ecc56954da9
ChuchoAraujo <jeseazuara@gmail.com>
2023-04-14 08:45:45 +0000

    relantionship

--- src/api/models.py
+++ src/api/models.py
@@ -8,7 +8,6 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=True)
 
-
     def __init__(self, user_name, email, password):
         self.user_name = user_name
         self.email = email
@@ -16,10 +15,11 @@ class User(db.Model):
         self.is_active = True
 
     def serialize(self):
+        favorites_list = [f.serialize() for f in self.favorites] # Se crea una lista de los favs y despues se llama en el serialize
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorites": favorites_list ## Se manda en el serialize toda la tabla favorites
         }
 
 class People(db.Model):
@@ -64,15 +64,14 @@ class Favorites(db.Model):
     people_id = db.Column(db.Integer, db.ForeignKey('people.id')) ## id referencia
     planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))  
     
-    user = db.relationship('User', backref='favorites')
-    planet = db.relationship('Planet', backref='favorites')
-    people = db.relationship('People', backref='favorites')
+    user = db.relationship('User', backref='favorites') # relacion tabla
+    planet = db.relationship('Planet', backref='favorites') # relacion tabla
+    people = db.relationship('People', backref='favorites') # relacion tabla
 
     def serialize(self):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "people_id": self.people_id,
-            "planet_id": self.planet_id,
-            # do not serialize the password, its a security breach
+            "people": self.people.serialize() if self.people else None, # Si el personaje existe devuelve todos los datos
+            "planet": self.planet.serialize() if self.planet else None  # Si el planeta existe devuelve todos los datos
         }
